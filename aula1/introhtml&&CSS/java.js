19184115a0a105c134e4df0d59e86723b13b3140
Davi Cabral <162502607+CabralScript@users.noreply.github.com>
2024-05-20 18:22:03 -0300

    att

---
+++ aula1/introhtml&&CSS/java.js
@@ -0,0 +1,7 @@
+//Introdução Java Script.
+
+//A linguagem de programação mais utilizada entre os desenvolvedores.
+//Linguagem mais utilizada em aplicações front-end, back-end e mobile.
+//Os browsers (navegadores) executam o JavaScript.
+//Existem inúmeros frameworksque se baseiam no JavaScript => React, React Native, Angular, Vue, entre outros.
+
